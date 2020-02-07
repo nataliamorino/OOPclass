29c42ffa8dd1f2daad8a5be33f7ab1fb89026a7d
nataliamorino <morino.natalia@gmail.com>
2020-02-07 12:01:49 -0500

    Add project files.

---
+++ OOPclass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOPclass
+{
+    class Car
+    {
+        public string color;
+        public int maxSpeed;
+        public string model;
+        public int year;
+        public void fullThrottle();
+
+        public Car(string modelName, string modelColor, int modelYear)
+        {
+        model= modelName;
+        color= modelColor;
+        year = modelYear;
+        }
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "Red", 1969);
+            Console.WriteLine(Ford.color + " " + Ford.year + " " + Ford.model);
+        }
+    }
+}
+
+
+            
+        
+
+    
+    
+    
+ 
